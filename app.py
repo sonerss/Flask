1da4bd9480b52c2f9730d893f5e86d10b7a3edc1
sonerss <sonerss@gmail.com>
2023-03-11 14:06:47 +0300

    create routing and add page1, page2 files

--- app.py
+++ app.py
@@ -3,3 +3,20 @@
 # Date : 2023-03-11
 # Aim : Flask BluePrint Training
 #-----------------------------------------------------------------
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def sayfa2():
+    return render_template('page2.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
